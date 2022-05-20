0d7ca637f32f93d7efd65d4bd6439429253aed39
simonsinfo <simonhemrom.code@gmail.com>
2022-05-20 11:14:47 +0600

    updated the project compon header

--- compons/projects.js
+++ compons/projects.js
@@ -1,18 +1,31 @@
+import { projects } from "../configs";
 import styled from "styled-components";
 
-const StyledProjects = styled.section`
+const StyledProjects = styled.div`
   width: 1000px;
   margin: 0px auto;
-  padding: 100px 0px;
+  padding: 50px 0px;
+  .projects-header {
+    text-align: center;
+    .small-title {
+      color: var(--green);
+      letter-spacing: 4px;
+    }
+  }
+  .projects-content {
+  }
 `;
 
 export default function Projects() {
+  console.log("data", projects);
+
   return (
     <StyledProjects>
-      <h2 className="small-heading">Other Noteworthy Projects</h2>
-      <div className="">
-        <h4>This is projects section</h4>
+      <div className="projects-header">
+        <h2 className="small-heading">Other Noteworthy Projects</h2>
+        <p className="small-title">View the Archive</p>
       </div>
+      <div className="projects-content"></div>
     </StyledProjects>
   );
 }
--- configs/projects.js
+++ configs/projects.js
@@ -1,6 +1,83 @@
-const projects = {
-  type: "projects-data",
-  name: "projects.json file",
-};
+const projects = [
+  {
+    id: 1,
+    name: "Find food recipe live videos (search)",
+    description:
+      "If you want to make a special kind of food for a restaurant or home but you don't know what you have to do to make it, you can search for that item and find its live videos.",
+    website: "https://msimon.netlify.app",
+    github: "https://github.com/spsimon-dev-tools/food-search",
+    technologies: [
+      { id: 1, tech: "React" },
+      { id: 2, tech: "Express" },
+      { id: 3, tech: "Node.js" },
+    ],
+  },
+  {
+    id: 2,
+    name: "BD-desh tourism agency (travels)",
+    description:
+      "You just have to be more discriminating with the help you render toward other people. You just have to be more discriminating with the help you render toward other people. Then this website can work well for them for booking.",
+    website: "https://msimon.netlify.app",
+    github: "https://github.com/spsimon-dev-tools/desh-tourism",
+    technologies: [
+      { id: 1, tech: "React" },
+      { id: 2, tech: "Express" },
+      { id: 3, tech: "Node.js" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Hotel-x public company (realstate)",
+    description:
+      "You have left your city for work or travel to another city. You may need to rent a house to stay but of course you can't decide how to manage it. Then by logging in to this website you will be able to book the room as per your requirement",
+    website: "https://msimon.netlify.app",
+    github: "https://github.com/spsimon-dev-tools/hotel-x",
+    technologies: [
+      { id: 1, tech: "React" },
+      { id: 2, tech: "Express" },
+      { id: 3, tech: "Node.js" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Team network (care-center)",
+    description:
+      "If you want to manage some people for a party or event then you can easily manage the people as per your need by logging in to this web page.",
+    website: "https://msimon.netlify.app",
+    github: "https://github.com/spsimon-dev-tools/volunteer-team",
+    technologies: [
+      { id: 1, tech: "React" },
+      { id: 2, tech: "Express" },
+      { id: 3, tech: "Node.js" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Deshboard (source-control)",
+    description:
+      "Imagine that you have a personal website and you can't manage where and how. In that case this dashboard can give you some great features. Such as users, products, create, update, delete.",
+    website: "https://msimon.netlify.app",
+    github: "https://github.com/spsimon-dev-tools/dashboard",
+    technologies: [
+      { id: 1, tech: "React" },
+      { id: 2, tech: "Express" },
+      { id: 3, tech: "Node.js" },
+    ],
+  },
+  {
+    id: 6,
+    name: "New fearures (chat-board)",
+    description:
+      "You want to have a conversation with one of your personal people or team but you do not want to share it with anyone but them. I am creating a simple chat application according to that feature. You can follow me to get updates.",
+    website: "https://msimon.netlify.app",
+    github: "#0",
+    technologies: [
+      { id: 1, tech: "React" },
+      { id: 2, tech: "Express" },
+      { id: 3, tech: "Node.js" },
+      { id: 4, tech: "API" },
+    ],
+  },
+];
 
 export default projects;
