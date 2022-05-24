a828849b3a8083cef33f0aee3ccc1c67c83d87fd
simonsinfo <simonhemrom.code@gmail.com>
2022-05-24 14:36:05 +0600

    updated the compon data and styles

--- layout/social.js
+++ layout/social.js
@@ -2,45 +2,62 @@ import { socials } from "../configs";
 import styled from "styled-components";
 import { Icon } from "../configs/icons";
 
-const StyledSocial = styled.div`
+const StyledSocialList = styled.div`
   position: fixed;
   left: 50px;
   bottom: -10px;
-  text-align: center;
-  a {
-    display: block;
-    width: 20px;
-    height: 20px;
-    margin-bottom: 30px;
-    &:hover {
-      transform: var(--translateY);
-      transition: var(--transition);
+  .social-items {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+
+    &:after {
+      content: "";
+      display: block;
+      width: 1px;
+      height: 90px;
+      margin: 0 auto;
+      background: var(--light-slate);
     }
-  }
-  &::after {
-    content: "";
-    width: 2px;
-    height: 90px;
-    display: inline-block;
-    background: var(--light-slate);
+
+    li {
+      &:last-of-type {
+        margin-bottom: 20px;
+      }
+
+      a {
+        padding: 10px;
+
+        &:hover,
+        &:focus {
+          transform: translateY(-3px);
+        }
+
+        svg {
+          width: 20px;
+          height: 20px;
+        }
+      }
+    }
   }
 `;
 
 export default function Social() {
   return (
-    <StyledSocial>
-      {socials &&
-        socials.map(({ name, url }, index) => (
-          <a
-            key={index}
-            href={url}
-            target="_blank"
-            rel="noopenner noreferrer"
-            aria-label={name}
-          >
-            <Icon name={name} />
-          </a>
-        ))}
-    </StyledSocial>
+    <StyledSocialList>
+      <ul className="social-items">
+        {socials &&
+          socials.map(({ name, url }, i) => (
+            <li key={i}>
+              <a href={url} aria-label={name} target="_blank" rel="noreferrer">
+                <Icon name={name} />
+              </a>
+            </li>
+          ))}
+      </ul>
+    </StyledSocialList>
   );
 }
